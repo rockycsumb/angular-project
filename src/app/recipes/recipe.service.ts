2da16a60cbc487a783b257f7d8c87d0da4b2c287
rockycsumb <romoreno@csumb.edu>
2021-01-29 15:55:59 +0000

    added recipe child routes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -31,6 +31,10 @@ export class RecipeService {
   getRecipes(){
 	  return this.recipes.slice();
   }
+
+  getRecipeById(id: number){
+	  return this.recipes[id];
+  }
 	
   addIngredientsToShoppingList(ingredients: Ingredient[]){
 	  // this.recipes.push(ingredients);
